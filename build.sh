69576302995e070c2af41a0857cd358324f278c4
Auston Stewart <auston@shedbuilt.net>
2018-05-30 08:40:40 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Configure
 ./configure --prefix=/usr        \
             --localstatedir=/var \
             --disable-logger     \
@@ -7,10 +8,12 @@
             --disable-rexec      \
             --disable-rlogin     \
             --disable-rsh        \
-            --disable-servers
-make -j $SHED_NUM_JOBS
-make DESTDIR=${SHED_FAKE_ROOT} install
-mkdir -v ${SHED_FAKE_ROOT}/bin
-mv -v ${SHED_FAKE_ROOT}/usr/bin/{hostname,ping,ping6,traceroute} ${SHED_FAKE_ROOT}/bin
-mkdir -v ${SHED_FAKE_ROOT}/sbin
-mv -v ${SHED_FAKE_ROOT}/usr/bin/ifconfig ${SHED_FAKE_ROOT}/sbin
+            --disable-servers &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+# Rearrange
+mkdir -v "${SHED_FAKE_ROOT}/bin" &&
+mv -v "${SHED_FAKE_ROOT}"/usr/bin/{hostname,ping,ping6,traceroute} "${SHED_FAKE_ROOT}/bin" &&
+mkdir -v "${SHED_FAKE_ROOT}/sbin" &&
+mv -v "${SHED_FAKE_ROOT}/usr/bin/ifconfig" "${SHED_FAKE_ROOT}/sbin"
